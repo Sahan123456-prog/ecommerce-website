6664b81b31af4db0cfdc9b832007b915ffa1d6c6
Sahan Chamodya <sahanchamodya865@gmail.com>
2026-09-05 00:36:08 +0530

    add to cart buttons

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ let cartCount = 0;
 const cartCountDisplay = document.getElementById("cart-count");
 
 const addCartButtons = document.getElementsByClassName(".add-cart");
+
+addCartButtons.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+        cartCount++;
+
+        cartCountDisplay.textContent = cartCount;
+
+        button.textContent = "Added ✓";
+
+        setTimeout(function () {
+
+            button.textContent = "Add to Cart";
+
+        }, 1000);
+
+    });
+
+});
