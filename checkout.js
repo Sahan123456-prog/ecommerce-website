ec10f15d19c8ebfc649482f80e7bca9d1b1cc712
Sahan Chamodya <sahanchamodya865@gmail.com>
2026-09-08 09:15:39 +0530

    add display order

--- checkout.js
+++ checkout.js
@@ -12,3 +12,71 @@ const cartCount =
 
 const checkoutForm =
     document.getElementById("checkout-form");
+
+function dispalyOrder() {
+
+    checkoutItems.innerHTML = "";
+
+    let total = 0;
+    let totalQuantity = 0;
+
+    if(cart.length === 0) {
+
+        checkoutItems.innerHTML = 
+            "<p>Your cart is empty.</p>";
+
+        checkoutTotal.textContent = "0";
+        cartCount.textContent = "0";
+
+        return;
+
+    }
+
+    cart.forEach(function(item) {
+
+        total += item.price * item.quantity;
+
+        totalQuantity += item.quantity;
+
+        const orderItem = 
+            document.createElement("div");
+
+        orderItem.classList.add(
+            "checkout-item"
+        );
+
+        orderItem.innerHTML = `
+
+            <div>
+
+                <h4>
+                    ${item.name}
+                </h4>
+
+                <p>
+                    $${item.price}
+                    ×
+                    ${item.quantity}
+                </p>
+
+            </div>
+
+            <strong>
+                $${item.price * item.quantity}
+            </strong>
+
+        `;
+
+        checkoutItems.appendChild(
+            orderItem
+        );
+
+    });
+
+    checkoutTotal.textContent = 
+        total;
+
+    cartCount.textContent = 
+        totalQuantity;
+        
+}
